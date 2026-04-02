f77b6b0e2aa671cacd9d16b68829e253ea996985
axel0302 <lefaucheuraxel@gmail.com>
2026-04-02 22:09:11 +0200

    regle de confidentialitée

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/privacy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy.html"));
+});
+
 function getRedirectUri(req) {
   if (process.env.REDIRECT_URI) return process.env.REDIRECT_URI;
 
